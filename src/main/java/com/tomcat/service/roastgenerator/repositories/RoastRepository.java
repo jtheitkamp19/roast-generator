8222993cec1bc2dd9e95e8c3ae6c00c3c8de3045
Jordan Heitkamp <j-heitkamp@onu.edu>
2022-02-09 23:47:14 -0500

    Fixes Unit tests

--- src/main/java/com/tomcat/service/roastgenerator/repositories/RoastRepository.java
+++ src/main/java/com/tomcat/service/roastgenerator/repositories/RoastRepository.java
@@ -3,10 +3,9 @@ package com.tomcat.service.roastgenerator.repositories;
 import com.tomcat.service.roastgenerator.models.Roast;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
+@Repository
 public interface RoastRepository extends JpaRepository<Roast, Long> {
     @Query(value = "SELECT COUNT(*) FROM roast")
     long getRoastCount();
